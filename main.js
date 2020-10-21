74172ddf0045f2b7db7c7f36a65368d1ab4bc955
Krishnachauhan65011 <71706960+Krishna65011@users.noreply.github.com>
2020-10-21 18:14:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+sound_status = "";
+sound_status2 = "";
+function preload()
+{
+    sound_harry = loadSound("harry.mp3");
+    sound_peter = loadSound("music2.mp3");
+}
+
 function setup()
 {
     canvas = createCanvas(600, 500);
@@ -24,6 +34,34 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+    sound_status =  sound_harry.isPlaying();
+    sound_status = sound_peter.isPlaying();
+    fill("#FF0000");
+    stroke("#FF0000");
+    
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+        sound_harry.stop();
+    }
+
+    if(sound_status2 == "false")
+    {
+        sound_peter.play();
+        document.getElementById("song_name").innerHTML = "Song playing = Peter Pan Theme song";
+    }
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        sound_peter.stop();
+    }
+
+    if(sound_status == "false")
+    {
+        sound_harry.play();
+        document.getElementById("song_name").innerHTML = "Song playing = Harry Poter theme song"; 
+    }
 }
 
 function gotPoses(results)
@@ -31,6 +69,8 @@ function gotPoses(results)
     if(results.length > 0);
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("left wrist x = " + leftWristX + " y = " + leftWristY);
